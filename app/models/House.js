ef3cc30d7f19b13842f4f0c2e03507bcfc46d3ed
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-03 15:48:43 -0600

    added model properties to match database @ sandbox

--- app/models/House.js
+++ app/models/House.js
@@ -1,7 +1,28 @@
 export class House {
   constructor(data) {
-
+    this.bedrooms = data.bedrooms
+    this.bathrooms = data.bathrooms
+    this.levels = data.levels
+    this.imgUrl = data.imgUrl
+    this.year = data.year
+    this.price = data.price
+    this.description = data.description || "No description provided."
+    this.creatorId = data.creatorId
 
   }
 
 }
+
+
+//NOTE - Properties pulled from https://sandbox.codeworksacademy.com/
+// bedrooms: Number, required
+// bathrooms: Number, required
+// levels: Number, required
+// imgUrl: String,
+// year: Number, required
+// price: Number, required
+// description: String,
+// creatorId: SchemaObjectId, required
+// *creator: Object (Virtual Added by Database)
+// *createdAt: ISO Timestamp (Virtual Added by Database)
+// *updatedAt: ISO Timestamp (Virtual Added by Database)
--- app/services/HousesService.js
+++ app/services/HousesService.js
@@ -1,12 +1,13 @@
 import { AppState } from "../AppState.js";
 import { House } from "../models/House.js";
+import { api } from "./AxiosService.js";
 
 
 class HousesService {
   async getHouses() {
     console.time('timer')
-    const response = await api.get('api/houses')
-    console.log('🏘️📡');
+    const response = await api.get('api/house')
+    console.log('🏘️📡', response.data)
 
     console.timeEnd('timer')
 
