a6f4bc5ae2691a9422ee28584fae9924cb2680d8
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-03 16:12:00 -0600

    +drawHouses() to controller

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,11 +1,23 @@
 import { AppState } from "../AppState.js";
+import { api } from "../services/AxiosService.js";
 import { housesService } from "../services/HousesService.js";
 import { Pop } from "../utils/Pop.js";
+import { setHTML } from "../utils/Writer.js";
 
 
 export class HousesController {
   constructor() {
     console.log('🏠🎮');
+
+
+  }
+
+  drawHouses() {
+    console.log('✏️🏡');
+    const houses = AppState.houses
+    let housesHTML = ''
+    houses.forEach(house => housesHTML += house.cardHTMLTemplate)
+    setHTML('houses-listing', housesHTML)
 
 
   }
